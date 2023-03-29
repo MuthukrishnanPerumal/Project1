1f17de73128681eaac0230b2c651c1cfbe16f504
ELCOT <ELCOT@parasuram>
2023-03-29 16:38:10 +0530

    Login Page

---
+++ Login_With_Registration-1/src/main/java/com/Registration/LoginWithRegistration1Application.java
@@ -0,0 +1,13 @@
+package com.Registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithRegistration1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithRegistration1Application.class, args);
+	}
+
+}
